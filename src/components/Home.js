37b6e7594f4bfebe0972990f7e1a5f0101a04ddf
Aashish Singla <aashishsingla567@gmail.com>
2022-09-16 13:06:42 +0530

    First commit

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Button } from '@mui/material';
+
+export default function Home() {
+    return (
+        <div>
+            <Button variant="contained">Hello World</Button>
+        </div>
+    );
+}
+
